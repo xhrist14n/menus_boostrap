7c9e215d0ad010350240b1851982ed54da5f2257
khrist14n <khrist14n@gmail.com>
2016-09-22 22:50:44 -0500

    project menus bootstrap

---
+++ git.sh
@@ -0,0 +1,19 @@
+clear
+git add ./test
+git add ./app
+git add ./app/scripts/.
+git add ./app/styles/.
+git add ./app/images/.
+git add ./app/fonts/.
+git add bower.json
+git add generate_all.sh
+git add git.sh
+git add Gruntfile.js
+git add package.json
+git add .bowerrc
+git add  .editorconfig
+git add  .gitattributes
+git add  .gitignore
+git add .yo-rc.json
+git commit -am "project menus bootstrap"
+git push origin master
