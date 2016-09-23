6f05101d3818d2417082b32050e8a672ef418df9
khrist14n <khrist14n@gmail.com>
2016-09-22 23:56:03 -0500

    project menus bootstrap

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(
+  function(){
+    var attr_class=$(".navbar").attr("class");
+    var splitted = attr_class.split("-");
+    var color_class = splitted[1];
+    $(".navbar").find("a").addClass("color-"+color_class);
+  }
+);
